b0d81168f2f005cacf6eef5ff24d3290d6fd6f67
Sakari Lehtonen <sakari@psitriangle.net>
2019-08-28 01:15:06 +0300

    Draw triangle using lines instead

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ extern crate piston_window;
 extern crate sprite;
 extern crate find_folder;
 
-use std::f32::{self, consts};
+use std::f64::{ consts };
 use piston_window::*;
 use cgmath::*;
 
-fn calc_poly_vertex(num_points:u32, angle:f32, radius:f32, vertex_index:u32) -> Point2<f32> {
+fn calc_poly_vertex(num_points:u32, angle:f64, radius:f64, vertex_index:u32) -> Point2<f64> {
 	let idx = num_points - vertex_index;
-	let angle_rad = angle.to_radians() + (2.0*consts::PI / num_points as f32) * idx as f32;
+	let angle_rad = angle.to_radians() + (2.0*consts::PI / num_points as f64) * idx as f64;
 
 	let x = angle_rad.sin() * radius;
 	let y = angle_rad.cos() * radius;
@@ -28,26 +28,31 @@ fn main() {
         .build()
         .unwrap();
 
-    let tri_pos = Point2::new(win_size[0]/2.0, win_size[1]/2.0);
+    let pos = Point2::new(win_size[0]/2.0, win_size[1]/2.0);
     let color = [1.0, 1.0, 1.0, 1.0];
     let num_points = 3;
     let angle = 60.0;
     let radius = 160.0;
+
     let poly = [
         calc_poly_vertex(num_points, angle, radius, 0),
         calc_poly_vertex(num_points, angle, radius, 1),
         calc_poly_vertex(num_points, angle, radius, 2),
     ];
 
+    let p0 = Point2::new(pos.x + poly[0].x, pos.y + poly[0].y);
+    let p1 = Point2::new(pos.x + poly[1].x, pos.y + poly[1].y);
+    let p2 = Point2::new(pos.x + poly[1].x, pos.y + poly[1].y);
+    let p3 = Point2::new(pos.x + poly[2].x, pos.y + poly[2].y);
+    let line_radius = 1.5;
+
     while let Some(event) = window.next() {
         window.draw_2d(&event, |ctx, gfx, _device| {
             clear([0.05, 0.05, 0.05, 1.0], gfx);
-
-            polygon(color, &[
-                [tri_pos.x + poly[0].x as f64, tri_pos.y + poly[0].y as f64],
-                [tri_pos.x + poly[1].x as f64, tri_pos.y + poly[1].y as f64],
-                [tri_pos.x + poly[2].x as f64, tri_pos.y + poly[2].y as f64],
-            ], ctx.transform, gfx);
+            line(color, line_radius, [p0.x, p0.y, p1.x, p1.y], ctx.transform, gfx);
+            line(color, line_radius, [p1.x, p1.y, p2.x, p2.y], ctx.transform, gfx);
+            line(color, line_radius, [p2.x, p2.y, p3.x, p3.y], ctx.transform, gfx);
+            line(color, line_radius, [p3.x, p3.y, p0.x, p0.y], ctx.transform, gfx);
         });
      }
 }
