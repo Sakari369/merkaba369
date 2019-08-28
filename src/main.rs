822f3c343ce4cf1f49742e1c6072a39f0a4ae2ed
Sakari Lehtonen <sakari@psitriangle.net>
2019-08-28 12:01:15 +0300

    Draw text

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,19 @@ fn main() {
 
     let win_size = [1280.0, 720.0];
 
-    let mut window: PistonWindow = WindowSettings::new("Game", win_size)
+    let mut window: PistonWindow = WindowSettings::new("369", win_size)
         .exit_on_esc(true)
         .graphics_api(opengl)
         .vsync(true)
         .build()
         .unwrap();
 
-    let pos = Point2::new(win_size[0]/2.0, win_size[1]/2.0);
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets").unwrap();
+    println!("{:?}", assets);
+    let mut glyphs = window.load_font(assets.join("iosevka-term-regular.ttf")).unwrap();
+
+    let origo = Point2::new(win_size[0]/2.0, win_size[1]/2.0);
     let color = [1.0, 1.0, 1.0, 1.0];
     let num_points = 3;
     let angle = 60.0;
@@ -40,19 +45,30 @@ fn main() {
         calc_poly_vertex(num_points, angle, radius, 2),
     ];
 
-    let p0 = Point2::new(pos.x + poly[0].x, pos.y + poly[0].y);
-    let p1 = Point2::new(pos.x + poly[1].x, pos.y + poly[1].y);
-    let p2 = Point2::new(pos.x + poly[1].x, pos.y + poly[1].y);
-    let p3 = Point2::new(pos.x + poly[2].x, pos.y + poly[2].y);
-    let line_radius = 1.5;
+    let line_radius = 1.2;
+    let font_size = 38.0;
+    let txt = text::Text::new_color([1.0, 1.0, 1.0, 1.0], font_size as u32);
 
     while let Some(event) = window.next() {
-        window.draw_2d(&event, |ctx, gfx, _device| {
+        window.draw_2d(&event, |ctx, gfx, device| {
             clear([0.05, 0.05, 0.05, 1.0], gfx);
-            line(color, line_radius, [p0.x, p0.y, p1.x, p1.y], ctx.transform, gfx);
-            line(color, line_radius, [p1.x, p1.y, p2.x, p2.y], ctx.transform, gfx);
-            line(color, line_radius, [p2.x, p2.y, p3.x, p3.y], ctx.transform, gfx);
-            line(color, line_radius, [p3.x, p3.y, p0.x, p0.y], ctx.transform, gfx);
+
+            let origo_trans = ctx.transform.trans(origo.x, origo.y);
+            line(color, line_radius, [poly[0].x, poly[0].y, poly[1].x, poly[1].y], origo_trans, gfx);
+            line(color, line_radius, [poly[1].x, poly[1].y, poly[2].x, poly[2].y], origo_trans, gfx);
+            line(color, line_radius, [poly[2].x, poly[2].y, poly[0].x, poly[0].y], origo_trans, gfx);
+
+            let trans3 = origo_trans.trans(poly[1].x - 32.0, poly[1].y + 16.0);
+            txt.draw("3", &mut glyphs, &ctx.draw_state, trans3, gfx).unwrap();
+
+            let trans6 = origo_trans.trans(poly[2].x - 11.0, poly[2].y - 16.0);
+            txt.draw("6", &mut glyphs, &ctx.draw_state, trans6, gfx).unwrap();
+
+            let trans9 = origo_trans.trans(poly[0].x + 9.0, poly[0].y + 16.0);
+            txt.draw("9", &mut glyphs, &ctx.draw_state, trans9, gfx).unwrap();
+
+            // Update glyphs before rendering.
+            glyphs.factory.encoder.flush(device);
         });
      }
 }
