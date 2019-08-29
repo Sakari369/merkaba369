12be122d59b0c5146fbdd0e90e229f319cef89c2
Sakari Lehtonen <sakari@psitriangle.net>
2019-08-29 13:15:47 +0300

    Replace text with sprites

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ extern crate find_folder;
 use std::f64::{ consts };
 use piston_window::*;
 use cgmath::*;
+use sprite::*;
+use std::rc::Rc;
+
+use ai_behavior::{
+    Action,
+    Sequence,
+    Wait,
+    WaitForever,
+    While
+};
 
 fn calc_poly_vertex(num_points:u32, angle:f64, radius:f64, vertex_index:u32) -> Point2<f64> {
 	let idx = num_points - vertex_index;
@@ -31,7 +41,31 @@ fn main() {
     let assets = find_folder::Search::ParentsThenKids(3, 3)
         .for_folder("assets").unwrap();
     println!("{:?}", assets);
-    let mut glyphs = window.load_font(assets.join("iosevka-term-regular.ttf")).unwrap();
+
+    let mut scene = Scene::new();
+    let mut texture_context = TextureContext {
+        factory: window.factory.clone(),
+        encoder: window.factory.create_command_buffer().into()
+    };
+
+    let t3 = Rc::new(Texture::from_path(
+        &mut texture_context,
+        assets.join("3.png"),
+        Flip::None,
+        &TextureSettings::new()
+    ).unwrap());
+    let t6 = Rc::new(Texture::from_path(
+        &mut texture_context,
+        assets.join("6.png"),
+        Flip::None,
+        &TextureSettings::new()
+    ).unwrap());
+    let t9 = Rc::new(Texture::from_path(
+        &mut texture_context,
+        assets.join("9.png"),
+        Flip::None,
+        &TextureSettings::new()
+    ).unwrap());
 
     let origo = Point2::new(win_size[0]/2.0, win_size[1]/2.0);
     let color = [1.0, 1.0, 1.0, 1.0];
@@ -45,30 +79,34 @@ fn main() {
         calc_poly_vertex(num_points, angle, radius, 2),
     ];
 
-    let line_radius = 1.2;
-    let font_size = 38.0;
-    let txt = text::Text::new_color([1.0, 1.0, 1.0, 1.0], font_size as u32);
+    let number_scale = 0.20;
+    let mut s3 = Sprite::from_texture(t3.clone());
+    s3.set_position(origo.x + poly[1].x - 32.0, origo.y + poly[1].y);
+    s3.set_scale(number_scale, number_scale);
+    scene.add_child(s3);
+
+    let mut s6 = Sprite::from_texture(t6.clone());
+    s6.set_position(origo.x + poly[2].x, origo.y + poly[2].y - 38.0);
+    s6.set_scale(number_scale, number_scale);
+    scene.add_child(s6);
+
+    let mut s9 = Sprite::from_texture(t9.clone());
+    s9.set_position(origo.x + poly[0].x + 33.0, origo.y + poly[0].y);
+    s9.set_scale(number_scale, number_scale);
+    scene.add_child(s9);
+
+    let line_radius = 1.5;
 
     while let Some(event) = window.next() {
         window.draw_2d(&event, |ctx, gfx, device| {
             clear([0.05, 0.05, 0.05, 1.0], gfx);
 
+            scene.draw(ctx.transform, gfx);
+
             let origo_trans = ctx.transform.trans(origo.x, origo.y);
             line(color, line_radius, [poly[0].x, poly[0].y, poly[1].x, poly[1].y], origo_trans, gfx);
             line(color, line_radius, [poly[1].x, poly[1].y, poly[2].x, poly[2].y], origo_trans, gfx);
             line(color, line_radius, [poly[2].x, poly[2].y, poly[0].x, poly[0].y], origo_trans, gfx);
-
-            let trans3 = origo_trans.trans(poly[1].x - 32.0, poly[1].y + 16.0);
-            txt.draw("3", &mut glyphs, &ctx.draw_state, trans3, gfx).unwrap();
-
-            let trans6 = origo_trans.trans(poly[2].x - 11.0, poly[2].y - 16.0);
-            txt.draw("6", &mut glyphs, &ctx.draw_state, trans6, gfx).unwrap();
-
-            let trans9 = origo_trans.trans(poly[0].x + 9.0, poly[0].y + 16.0);
-            txt.draw("9", &mut glyphs, &ctx.draw_state, trans9, gfx).unwrap();
-
-            // Update glyphs before rendering.
-            glyphs.factory.encoder.flush(device);
         });
      }
 }
